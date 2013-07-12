20a6ebd1fa97aa8400215cbe4416509dc69e75ad
Jillian <noctekana@gmail.com>
2013-07-12 15:23:00 -0400

    Initial Commit

---
+++ Wallaby/WAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WAppDelegate.h
+//  Wallaby
+//
+//  Created by Jillian Crossley on 2013-07-12.
+//  Copyright (c) 2013 Jillian Crossley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Wallaby/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wallaby
+//
+//  Created by Jillian Crossley on 2013-07-12.
+//  Copyright (c) 2013 Jillian Crossley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WAppDelegate class]));
+    }
+}
